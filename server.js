ba8950871b5a9b46e249658ed6e3c3cf0c74939f
Bobby <rodLatulippe@hotmail.com>
2023-10-19 17:39:02 -0400

    Continuation de server.js

--- server.js
+++ server.js
@@ -1,39 +1,76 @@
 //imports
 import express from "express";
-import coursesDB from ".\domain\courses\courses.js";
+import courses from ".\domain\courses\courses.js";
+import students from ".\domain\students\students.js";
+import subscriptions from ".\domain\subscriptions\subscriptions.js";
 
 //variables
 const app = express();
 const PORT = 3000;
+
 
 //Main Page
 app.get("/", (req, res) => {
   res.send("The server is running");
 });
 
+
 //Courses
 app.get("/courses", (req, res) => {
-  res.send();
+  res.send(getCourses());
 });
 
 app.get("/courses/:courseId", (req, res) => {
-    res.send(req.params);
+    res.send(getCourses(req.params));
   });
 
 app.post("/courses/add", (req, res) => {
+    console.log("Insert the Json code that will make up your course");
+    const data = req.body;
+     
+    console.log("id: ", data.id);
+    console.log("Name: ", data.name);
+    
+    res.send();
+  });
+/*
+app.delete("/courses/remove/:courseId", (req, res) => {
     console.log(req.body);
     res.sendStatus(200);
   });
 
-app.delete("/courses/delete", (req, res) => {
+app.put("/courses/update/:courseId", (req, res) => {
     console.log(req.body);
     res.sendStatus(200);
   });
+
 
 //Students
+app.get("/students", (req, res) => {
+    res.send();
+  });
+
+app.get("/students/:studentId", (req, res) => {
+    res.send();
+  });
+
+
+app.post("/students/add", (req, res) => {
+    res.send();
+  });
 
 
 //Applications
+app.post("/subscriptions/add/:studentId/:courseId", (req, res) => {
+    res.send();
+  });
+
+app.delete("/subscriptions/remove/:studentId/:courseId", (req, res) => {
+    console.log(req.body);
+    res.sendStatus(200);
+  });
+*/
+//Listen
 app.listen(PORT, (error)=>{
     if(!error){
         console.log("Server has successfully started, and the application is listening on port " + PORT)
